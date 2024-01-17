524439a01f5ed097d2d2dc9de012581efb6b60a3
Vikas Mirani <vikasmirani@Vikass-MacBook-Pro.local>
2024-01-17 21:51:44 +0530

    package changes made

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ImageProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </ImageProvider>
